a3caa68467b4a2f766fc091fd88cd0cf2c79f173
NicholasLepage <nic.lep@hotmail.com>
2019-03-21 15:15:07 -0400

    changed app structure to be a package

---
+++ myapp/routes.py
@@ -0,0 +1,50 @@
+from flask import flash, redirect, render_template, url_for
+
+from myapp import app
+from myapp.forms import RegistrationForm, LoginForm
+from myapp.models import Post, User
+
+
+posts = [
+    {
+        'author': 'Nicholas Lepage',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 18th, 2019'
+    },
+    {
+        'author': 'Coby Griffith',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 19th, 2019'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'admin':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
